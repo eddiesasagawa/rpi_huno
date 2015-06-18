755bca9c5e3a1dfb424fbc93392fc75a27df55a5
Eddie Tio <arnev006@umn.edu>
2015-06-18 14:26:40 -0500

    First implementation of forward kinematics for all limbs.

---
+++ src/HunoForwardKinematics.cpp
@@ -0,0 +1,154 @@
+#ifndef HUNO_FK_CLASS
+#define HUNO_FK_CLASS
+
+#include "ros/ros.h"
+#include "sensor_msgs/JointState"
+#include <Eigen/Dense>
+
+class HunoForwardKinematics {
+ ////NOTE: Base frame is the Head frame!
+ public:
+ //Variables:
+ Eigen::Matrix4f gLFoot_0;
+ Eigen::Matrix4f gRFoot_0;
+ Eigen::Matrix4f gLHand_0;
+ Eigen::Matrix4f gRHand_0;
+
+ Eigen::Matrix<float,3,16> omega_all; // 3 x 16 matrix containing 16 column-vectors, each a unit vector of the corresponding joint twist axis
+ Eigen::Matrix<float,16,3> q_all; // 16 x 3 matrix containing 16 rows of points, each a 3D point on the joint twist axis
+ Eigen::Matrix<float,16,1> ref_angles; // Vector of reference angles for joints
+
+ const int num_joints;
+
+ //Functions:
+ //Constructor
+ HunoForwardKinematics():
+  num_joints(16)
+ {
+  //Set g_0 reference configuration matrices
+  gLFoot_0 << 1, 0, 0, -0.05375,
+              0, 1, 0, 0.0115,
+              0, 0, 1, -0.10615,
+              0, 0, 0, 1;
+  gRFoot_0 << 1, 0, 0, -0.05375,
+              0, 1, 0, -0.0115,
+              0, 0, 1, -0.10615,
+              0, 0, 0, 1;
+  gLHand_0 << 1, 0, 0, 0.0462,
+              0, 1, 0, 0.0672,
+              0, 0, 1, -0.07575,
+              0, 0, 0, 1;
+  gRHand_0 << 1, 0, 0, 0.0462,
+              0, 1, 0, -0.0672,
+              0, 0, 1, -0.07575,
+              0, 0, 0, 1;
+
+  //Set twist axes and points and reference angles
+  omega_all << -1,  0,  0,  0, -1, -1,  0,  0,  0, -1,  0, -1,  1,  0, -1,  1,
+                0, -1, -1, -1,  0,  0,  1,  1,  1,  0, -1,  0,  0,  1,  0,  0,
+                0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0;
+
+  q_all << -0.06245,  0.0115, -0.0699,
+           -0.02515,  0.0115, -0.0699,
+            0.02385,  0.0115, -0.0699,
+           -0.01545,  0.0115, -0.09515,
+           -0.05375,  0.0115, -0.09515,
+           -0.06245, -0.0115, -0.0699,
+           -0.02515, -0.0115, -0.0699,
+            0.02385, -0.0115, -0.0699,
+           -0.01545, -0.0115, -0.09515,
+           -0.05375, -0.0115, -0.09515,
+           -0.0263,   0.0292, -0.0266,
+           -0.0263,   0.0672, -0.0266,
+           -0.0263,   0.0672, -0.07575,
+           -0.0263,  -0.0292, -0.0266,
+           -0.0263,  -0.0672, -0.0266,
+           -0.0263,  -0.0672, -0.07575;
+  q_all.transposeInPlace(); //Transpose so that points are columns now for copying to vectors.
+
+  ref_angles << 134.5,
+                134.5,
+                224.5,
+                134.5,
+                134.5, //check theta_4
+                134.5, //check theta_5
+                134.5,
+                 44.5,
+                134.5,
+                134.5, //check theta_9
+                 44.5, //check theta_10
+                 44.5,
+                 44.5,
+                224.5, //check theta_13
+                224.5,
+                224.5;
+  ref_angles = ref_angles * (3.14/180); //Convert to radians
+
+ } //End constructor
+
+ // \brief Calculate exponential of angle theta about twist
+ // \param joint_id : ID of joint that is calculated for
+ // \param theta : angle of rotation about twist (RADIANS)
+ // \out   exp_xihat_theta : 4x4 matrix containing exponential matrix of twist
+ Eigen::Matrix4f exp_xihat_theta(int joint_id, float theta) {
+  Eigen::Vector3f omega = omega_all.col(joint_id);
+  Eigen::Vector3f q = q_all.row(joint_id);
+  float theta_corrected = theta - ref_angles(joint_id);
+
+  Eigen::Matrix3f omegahat;
+  omegahat << 0, -omega(3), omega(2),
+              omega(3), 0, -omega(1),
+              -omega(2), omega(1), 0;
+
+  Eigen::Matrix3f exp_omegahat_theta;
+  exp_omegahat_theta = Eigen::Matrix3f::Identity() + omegahat*sin(theta_corrected) + omegahat*omegahat*(1-cos(theta_corrected));
+
+  Eigen::Matrix4f out_exp_xihat_theta;
+  out_exp_xihat_theta.block<3,3>(0,0) = exp_omegahat_theta;
+  out_exp_xihat_theta.block<3,1>(0,3) = (Eigen::Matrix3f::Identity()-exp_omegahat_theta)*(-1*q); // Check that omegahat does not need to be used
+  out_exp_xihat_theta.row(3) << 0, 0, 0, 1;
+
+  return out_exp_xihat_theta;
+ }
+
+ //--Limb Forward Kinematics--
+ // \brief Four functions below that calculate FK for each limb
+ // \params thetaxx : angles for each joint in the limb (RADIANS)
+ // \out    FKmatrix: 4x4 homogeneous transformation matrix from Head frame to end of limb
+ Eigen::Matrix4f LeftFootFK(float theta00, float theta01, float theta02, float theta03, float theta04) {
+  Eigen::Matrix4f out_LFootFK;
+  out_LFootFK =   (exp_xihat_theta(0,theta00)) * (exp_xihat_theta(1,theta01))
+                * (exp_xihat_theta(2,theta02)) * (exp_xihat_theta(3,theta03))
+                * (exp_xihat_theta(4,theta04)) * gLFoot_0;
+
+  return out_LFootFK;
+ }
+
+ Eigen::Matrix4f RightFootFK(float theta05, float theta06, float theta07, float theta08, float theta09) {
+  Eigen::Matrix4f out_RFootFK;
+  out_RFootFK =   (exp_xihat_theta(5,theta05)) * (exp_xihat_theta(6,theta06))
+                * (exp_xihat_theta(7,theta07)) * (exp_xihat_theta(8,theta08))
+                * (exp_xihat_theta(9,theta09)) * gRFoot_0;
+
+  return out_RFootFK;
+ }
+
+ Eigen::Matrix4f LeftHandFK(float theta10, float theta11, float theta12) {
+  Eigen::Matrix4f out_LHandFK;
+  out_LHandFK =   (exp_xihat_theta(10,theta10)) * (exp_xihat_theta(11,theta11))
+                * (exp_xihat_theta(12,theta12)) * gLHand_0;
+
+  return out_LHandFK;
+ }
+
+ Eigen::Matrix4f RightHandFK(float theta13, float theta14, float theta15) {
+  Eigen::Matrix4f out_RHandFK;
+  out_RHandFK =   (exp_xihat_theta(13,theta13)) * (exp_xihat_theta(14,theta14))
+                * (exp_xihat_theta(15,theta15)) * gRHand_0;
+
+  return out_RHandFK;
+ }
+
+};
+
+#endif
--- src/huno_forwardkinematics.cpp
+++ src/huno_forwardkinematics.cpp
@@ -1,36 +1,172 @@
 #include "ros/ros.h"
 #include "sensor_msgs/JointState.h"
+#include "geometry_msgs/Pose.h"
+
+#include "HunoForwardKinematics.h"
+#include "rpi_huno/HunoLimbPoses.h"
 
 #include <Eigen/Dense>
 
 /* ===DESCRIPTION===
 This node is the forward kinematics node.
 
-Inputs:
+Subscribe To:
  sensor_msgs::JointState joint_current_angles
 
-Outputs:
- ?? RightHand position/orientation
- ?? LeftHand position/orientation
- ?? RightFoot position/orientation
- ?? LeftFoot position/orientation
+Publish:
+ geometry_msgs::Pose RightHand position/orientation
+ geometry_msgs::Pose LeftHand position/orientation
+ geometry_msgs::Pose RightFoot position/orientation
+ geometry_msgs::Pose LeftFoot position/orientation
 
 ===TO-DOs===
--Output message format TBD.
 -Calculate velocities and forces
 
 */
+
+#define DEG2RAD (3.14/180)
 
 class HunoFK {
  public:
  ros::NodeHandle &node;
  ros::Subscriber sJoint_angles;
+ ros::Publisher pHuno_poses;
 
+ HunoForwardKinematics huno_fwdkin;
+ Eigen::Matrix4f head_T_LFoot;
+ Eigen::Matrix4f head_T_RFoot;
+ Eigen::Matrix4f head_T_LHand;
+ Eigen::Matrix4f head_T_RHand;
+
+ //Constructor
  HunoFK(ros::NodeHandle &n):
   node(n),
-  sJoint_angles(node.subscribe("joint_odom",1,&HunoFK::runFK,this))
+  sJoint_angles(node.subscribe("joint_odom",1,&HunoFK::runFK,this)),
+  pHuno_poses(node.advertise<rpi_huno::HunoLimbPoses>("/huno_poses",1)),
+  huno_fwdkin(),
+  head_T_LFoot(Eigen::Matrix4f::Identity()),
+  head_T_RFoot(Eigen::Matrix4f::Identity()),
+  head_T_LHand(Eigen::Matrix4f::Identity()),
+  head_T_RHand(Eigen::Matrix4f::Identity())
  {
 
  }
 
-};
+ void publishFK() {
+  Eigen::Vector3f rot_axis = Eigen::Vector3f::Zero();
+  float rot_angle;
+  rpi_huno::HunoLimbPoses huno_poses;
+
+  huno_poses.header.stamp = ros::Time::now();
+
+  //Left foot
+  huno_poses.left_foot.position.x = head_T_LFoot(3,0);
+  huno_poses.left_foot.position.y = head_T_LFoot(3,1);
+  huno_poses.left_foot.position.z = head_T_LFoot(3,2);
+  rot_angle = acos( ( ((head_T_LFoot.block<3,3>(0,0)).trace())-1 )/2 ); //Back out rotation unit vector and angle from rotation matrix
+  if(sin(rot_angle) == 0) //Rotation matrix is identity and thus unit vector is arbitrary, or matrix was invalid such that it would cause divide by zero
+  { rot_axis = Eigen::Vector3f::Zero(); }
+  else
+  {
+   rot_axis(0) = ((head_T_LFoot(2,1)-head_T_LFoot(1,2))/(2*sin(rot_angle)));
+   rot_axis(1) = ((head_T_LFoot(0,2)-head_T_LFoot(2,0))/(2*sin(rot_angle)));
+   rot_axis(2) = ((head_T_LFoot(1,0)-head_T_LFoot(0,1))/(2*sin(rot_angle)));
+  }
+  huno_poses.left_foot.orientation.x = rot_axis(0)*sin(rot_angle/2);
+  huno_poses.left_foot.orientation.y = rot_axis(1)*sin(rot_angle/2);
+  huno_poses.left_foot.orientation.z = rot_axis(2)*sin(rot_angle/2);
+  huno_poses.left_foot.orientation.w = cos(rot_angle/2);
+
+  //Right foot
+  huno_poses.right_foot.position.x = head_T_RFoot(3,0);
+  huno_poses.right_foot.position.y = head_T_RFoot(3,1);
+  huno_poses.right_foot.position.z = head_T_RFoot(3,2);
+  rot_angle = acos( ( ((head_T_RFoot.block<3,3>(0,0)).trace())-1 )/2 ); //Back out rotation unit vector and angle from rotation matrix
+  if(sin(rot_angle) == 0) //Rotation matrix is identity and thus unit vector is arbitrary
+  { rot_axis = Eigen::Vector3f::Zero(); }
+  else
+  {
+   rot_axis(0) = ((head_T_RFoot(2,1)-head_T_RFoot(1,2))/(2*sin(rot_angle)));
+   rot_axis(1) = ((head_T_RFoot(0,2)-head_T_RFoot(2,0))/(2*sin(rot_angle)));
+   rot_axis(2) = ((head_T_RFoot(1,0)-head_T_RFoot(0,1))/(2*sin(rot_angle)));
+  }
+  huno_poses.right_foot.orientation.x = rot_axis(0)*sin(rot_angle/2);
+  huno_poses.right_foot.orientation.y = rot_axis(1)*sin(rot_angle/2);
+  huno_poses.right_foot.orientation.z = rot_axis(2)*sin(rot_angle/2);
+  huno_poses.right_foot.orientation.w = cos(rot_angle/2);
+
+  //Left hand
+  huno_poses.left_hand.position.x = head_T_LHand(3,0);
+  huno_poses.left_hand.position.y = head_T_LHand(3,1);
+  huno_poses.left_hand.position.z = head_T_LHand(3,2);
+  rot_angle = acos( ( ((head_T_LHand.block<3,3>(0,0)).trace())-1 )/2 ); //Back out rotation unit vector and angle from rotation matrix
+  if(sin(rot_angle) == 0) //Rotation matrix is identity and thus unit vector is arbitrary
+  { rot_axis = Eigen::Vector3f::Zero(); }
+  else
+  {
+   rot_axis(0) = ((head_T_LHand(2,1)-head_T_LHand(1,2))/(2*sin(rot_angle)));
+   rot_axis(1) = ((head_T_LHand(0,2)-head_T_LHand(2,0))/(2*sin(rot_angle)));
+   rot_axis(2) = ((head_T_LHand(1,0)-head_T_LHand(0,1))/(2*sin(rot_angle)));
+  }
+  huno_poses.left_hand.orientation.x = rot_axis(0)*sin(rot_angle/2);
+  huno_poses.left_hand.orientation.y = rot_axis(1)*sin(rot_angle/2);
+  huno_poses.left_hand.orientation.z = rot_axis(2)*sin(rot_angle/2);
+  huno_poses.left_hand.orientation.w = cos(rot_angle/2);
+
+  //Right hand
+  huno_poses.right_hand.position.x = head_T_RHand(3,0);
+  huno_poses.right_hand.position.y = head_T_RHand(3,1);
+  huno_poses.right_hand.position.z = head_T_RHand(3,2);
+  rot_angle = acos( ( ((head_T_RHand.block<3,3>(0,0)).trace())-1 )/2 ); //Back out rotation unit vector and angle from rotation matrix
+  if(sin(rot_angle) == 0) //Rotation matrix is identity and thus unit vector is arbitrary
+  { rot_axis = Eigen::Vector3f::Zero(); }
+  else
+  {
+   rot_axis(0) = ((head_T_RHand(2,1)-head_T_RHand(1,2))/(2*sin(rot_angle)));
+   rot_axis(1) = ((head_T_RHand(0,2)-head_T_RHand(2,0))/(2*sin(rot_angle)));
+   rot_axis(2) = ((head_T_RHand(1,0)-head_T_RHand(0,1))/(2*sin(rot_angle)));
+  }
+  huno_poses.right_hand.orientation.x = rot_axis(0)*sin(rot_angle/2);
+  huno_poses.right_hand.orientation.y = rot_axis(1)*sin(rot_angle/2);
+  huno_poses.right_hand.orientation.z = rot_axis(2)*sin(rot_angle/2);
+  huno_poses.right_hand.orientation.w = cos(rot_angle/2);
+
+  //Publish
+  pHuno_poses.publish(huno_poses);
+ }
+
+ void runFK(const sensor_msgs::JointState& joint_states) {
+  head_T_LFoot = huno_fwdkin.LeftFootFK((DEG2RAD*joint_states.position[0]),
+                                        (DEG2RAD*joint_states.position[1]),
+                                        (DEG2RAD*joint_states.position[2]),
+                                        (DEG2RAD*joint_states.position[3]),
+                                        (DEG2RAD*joint_states.position[4]));
+
+  head_T_RFoot = huno_fwdkin.RightFootFK((DEG2RAD*joint_states.position[5]),
+                                         (DEG2RAD*joint_states.position[6]),
+                                         (DEG2RAD*joint_states.position[7]),
+                                         (DEG2RAD*joint_states.position[8]),
+                                         (DEG2RAD*joint_states.position[9]));
+
+  head_T_LHand = huno_fwdkin.LeftHandFK((DEG2RAD*joint_states.position[10]),
+                                        (DEG2RAD*joint_states.position[11]),
+                                        (DEG2RAD*joint_states.position[12]));
+
+  head_T_RHand = huno_fwdkin.RightHandFK((DEG2RAD*joint_states.position[13]),
+                                         (DEG2RAD*joint_states.position[14]),
+                                         (DEG2RAD*joint_states.position[15]));
+  publishFK();
+ }
+
+}; //End class
+
+int main(int argc, char **argv) {
+ ros::init(argc, argv, "huno_forwardkinematics");
+ ros::NodeHandle n;
+
+ HunoFK huno_fk(n);
+
+ ros::spin();
+
+ return 1; //Shouldn't ever get here.
+}
